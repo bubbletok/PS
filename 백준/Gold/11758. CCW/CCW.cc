517574df7420f029c17ede6cf72f9db293341aab
bubbletok <62411634+bubbletok@users.noreply.github.com>
2024-03-25 23:31:05 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.cc
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -9,25 +9,20 @@ int main() {
 	cin >> px1 >> py1;
 	cin >> px2 >> py2;
 	cin >> px3 >> py3;
-	int ans;
 	int vx1 = px2-px1;
 	int vy1 = py2-py1;
-	int vx2 = px3-px2;
-	int vy2 = py3-py2;
-	int vvx1 = -vy1;
-	int vvy1 = vx1;
-	
-	double ddot = vvx1*vx2 + vvy1*vy2;
-	double llen = sqrt(vvx1*vvx1 + vvy1*vvy1) * sqrt(vx2*vx2 + vy2*vy2);
-	double _ccos = ddot/llen;
-	if(_ccos==0){
+	int vx2 = px2-px3;
+	int vy2 = py2-py3;
+	int mul = vx1*vy2 - vy1*vx2;
+	int ans;
+	if(mul==0){
 		ans = 0;
 	}
-	else if(_ccos > 0){
-		ans = 1;
+	else if(mul>0){
+		ans = -1;
 	}
 	else{
-		ans = -1;
+		ans = 1;
 	}
 	cout << ans;
 	return 0;
