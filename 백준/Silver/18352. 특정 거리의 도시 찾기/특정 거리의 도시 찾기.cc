327c63be9b44433e5d3af517aa3d46f96849f582
bubbletok <62411634+bubbletok@users.noreply.github.com>
2024-05-07 21:12:46 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 416 ms, Memory: 22400 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -3,25 +3,26 @@
 #include <queue>
 #define INF 999999999
 using namespace std;
-vector<pair<int,int>> graph[300001];
+vector<int> graph[300001];
 int n,m,k,x;
-void dijkstra(int start, vector<int>& dist, vector<pair<int,int>>* graph){
-	dist[start] = 0;
-	priority_queue<pair<int,int>> q;
-	q.push({0,start});
+vector<int> dist(300000+1,INF);
+vector<int> ans;
+bool check[300001] = {false};
+
+void dfs(int start){
+	queue<pair<int,int>> q;
+	check[start]=true;
+	q.push({start,0});
 	while(!q.empty()){
-		int cur_dist = -q.top().first;
-		int cur_node = q.top().second;
+		int node = q.front().first;
+		int cur_dist = q.front().second;
 		q.pop();
-		
-		for(int i=0; i<graph[cur_node].size(); i++){
-			int new_node = graph[cur_node][i].first;
-			int new_dist = cur_dist + graph[cur_node][i].second;
-			//cout << new_node << " " << new_dist << endl;
-			if(new_dist < dist[new_node]){
-				dist[new_node] = new_dist;
-				q.push({-new_dist,new_node});
-			}
+		dist[node] = cur_dist;
+		for(int i=0; i<graph[node].size(); i++){
+			int next = graph[node][i];
+			if(check[next]) continue;
+			q.push({next,cur_dist+1});
+			check[next] = true;
 		}
 	}
 }
@@ -34,11 +35,9 @@ int main() {
 	for(int i=0; i<m; i++){
 		int a,b,t;
 		cin >> a >> b;
-		graph[a].push_back({b,1});
+		graph[a].push_back(b);
 	}
-	vector<int> dist(n+1,INF);
-	dijkstra(x,dist,graph);
-	vector<int> ans;
+	dfs(x);
 	for(int i=1; i<=n; i++){
 		if(dist[i]==k){
 			ans.push_back(i);
