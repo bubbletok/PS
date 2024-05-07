9d1e64d4a1a3a65b50278294b0aeb535e5fe7050
bubbletok <62411634+bubbletok@users.noreply.github.com>
2024-05-07 20:56:28 +0900

    [Gold III] Title: 파티, Time: 4 ms, Memory: 2336 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -3,9 +3,10 @@
 #include <queue>
 #define INF 999999999
 using namespace std;
-vector<pair<int,int>> graph[1001];
+vector<pair<int,int>> nor_graph[1001];
+vector<pair<int,int>> rev_graph[1001];
 int n,m,x;
-void dijkstra(int start, vector<int>& dist){
+void dijkstra(int start, vector<int>& dist, vector<pair<int,int>> graph[]){
 	dist[start] = 0;
 	priority_queue<pair<int,int>> q;
 	q.push({0,start});
@@ -27,19 +28,26 @@ void dijkstra(int start, vector<int>& dist){
 }
 
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	cin >> n >> m >> x;
 	for(int i=0; i<m; i++){
 		int a,b,t;
 		cin >> a >> b >> t;
-		graph[a].push_back({b,t});
+		nor_graph[a].push_back({b,t});
+		rev_graph[b].push_back({a,t});
 	}
 	int ans = -1;
 	vector<int> backDist(n+1,INF);
-	dijkstra(x,backDist);
+	dijkstra(x,backDist,nor_graph);
+	vector<int> dist(n+1,INF);
+	dijkstra(x,dist,rev_graph);
 	for(int i=1; i<=n; i++){
-		vector<int> dist(n+1, INF);
-		dijkstra(i, dist);
-		ans = dist[x]+backDist[i] > ans ? dist[x]+backDist[i] : ans;
+		int val = backDist[i] + dist[i];
+		if(val>ans){
+			ans = val;
+		}
 	}
 	cout << ans;
 	return 0;
