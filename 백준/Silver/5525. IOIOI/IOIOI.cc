82b0a859a31a1cab664e31f7a1cdb63a2651884f
bubbletok <62411634+bubbletok@users.noreply.github.com>
2024-07-18 17:04:40 +0900

    [Silver I] Title: IOIOI, Time: 16 ms, Memory: 9000 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -1,37 +1,73 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 using namespace std;
 using ll = long long;
 int n, m;
-string s;
+vector<int> getPi(string& p) {
+	vector<int> pi(p.size(),0);
+	// KMP로 자기 자신을 찾는다.
+	int begin = 1;
+	int matched = 0; //현재 일치된 문자열 길이
+	while (begin + matched < p.size()) {
+		if (p[begin + matched] == p[matched]) {
+			++matched;
+			pi[begin+matched-1] = matched;
+		}
+		else {
+			if (matched == 0) {
+				++begin;
+			}
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
+	return pi;
+}
+
+vector<int> kmp(string& s, string& p) {
+	vector<int> idx;
+	vector<int> pi = getPi(p);
+	int begin = 0;
+	int matched = 0;
+
+	while (begin <= s.size() - p.size()) {
+		if (matched < p.size() && s[begin + matched] == p[matched]) {
+			++matched;
+			if (matched == p.size()) {
+				idx.push_back(begin);
+			}
+		}
+		else {
+			if (matched == 0) {
+				++begin;
+			}
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
+	return idx;
+}
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> m >> s;
-	int r = m;
-	ll ans = 0;
-	int len = (n + 1) + n;
-	for (int i = 0; i < m-len+1; i++) {
-		if (s[i] == 'O') continue;
-		bool found = true;
-		int idx = i;
-		for (int j = i; j < i + len-1; j++) {
-			if (s[j] == s[j + 1]) {
-				found = false;
-				break;
-			}
-			idx = j;
-		}
-		if (found) {
-			//cout << "i: " << i << endl;
-			ans++;
-		}
-		else {
-			i = idx;
-		}
+	cin >> n >> m;
+	string s;
+	cin >> s;
+	string p = "";
+	for (int i = 0; i < n; i++) {
+		p += 'I';
+		p += 'O';
 	}
-	cout << ans;
+	p += 'I';
+	vector<int> ans;
+	ans = kmp(s, p);
+	cout << ans.size();
 	return 0;
 }
