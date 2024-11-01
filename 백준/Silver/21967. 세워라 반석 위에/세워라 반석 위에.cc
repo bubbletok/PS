eb0131a919cc3a40fde1ae76aca54d068dcba315
bubbletok <62411634+bubbletok@users.noreply.github.com>
2024-11-01 22:06:49 +0900

    [Silver III] Title: 세워라 반석 위에, Time: 76 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/21967. 세워라 반석 위에/세워라 반석 위에.cc
+++ 백준/Silver/21967. 세워라 반석 위에/세워라 반석 위에.cc
@@ -7,6 +7,25 @@
 using namespace std;
 int n;
 int arr[1000001];
+int height[11];
+int _max;
+int _min;
+void GetMaxMin(){
+    int temp_max = 0;
+    for(int i=1; i<=10; i++){
+        if(height[i]>0){
+            temp_max = i;
+        }
+    }
+    int temp_min = 11;
+    for(int i=10; i>=1; i--){
+        if(height[i]>0){
+            temp_min = i;
+        }
+    }
+    _max = temp_max;
+    _min = temp_min;
+}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -16,27 +35,22 @@ int main() {
     for(int i=0; i<n; i++){
         cin >> arr[i];
     }
-    
     int ans = 0;
     int s = 0, e = 0;
-    multiset<int> ms;
-    ms.insert(arr[0]);
-    int _min = arr[0], _max = arr[0];
+    _max = arr[0];
+    _min = arr[0];
+    height[arr[0]]++;
     while(e<n&&s<n){
         if(abs(_max-_min)<=2){
-            ans = max(ans,e-s+1);
             e++;
-            ms.insert(arr[e]);
+            height[arr[e]]++;
         }
         else{
-            auto it = ms.find(arr[s]);
-            ms.erase(it);
+            height[arr[s]]--;
             s++;
         }
-        auto begin_it = ms.begin();
-        auto end_it = ms.end(); end_it--;
-        _min = *begin_it;
-        _max = *end_it;
+        GetMaxMin();
+        ans = max(ans, (e-s));
     }
     cout << ans;
     return 0;
